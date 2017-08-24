05aa6a522fa954adeb02b59014c5f03a49addedb
Chris De Lorenzo <delorenzoc@tabcorp.com.au>
2017-08-24 18:39:33 +1000

    update

--- fabfile.py
+++ fabfile.py
@@ -36,9 +36,8 @@ def stop_test():
 def git_pull():
     run('cd /home/centos/ao-client-pubnub && git pull', pty=False)
 
-@parallel
-def start_network_monitor(arg):
-    run('nohup sh -c \'while true; do ./network_monitor.sh; sleep 5; done\' &', pty=False)
+def start_network_monitor():
+    run('''nohup sh -c 'while true; do ./network_monitor.sh; sleep 5; done' &''', pty=False)
 
 @parallel
 def free():
